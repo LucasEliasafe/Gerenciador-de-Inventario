dd6d4328d209bc5669bb76b320d46d12b45c0b09
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-06 13:39:31 -0300

    Feat:criar um name, quantidade, preço para o produto

--- src/Product.java
+++ src/Product.java
@@ -1,8 +1,21 @@
 public class Product {
-    private String nome;
+    private String name;
     private int quantity;
     private double price;
 
 
+        @param name
+        @param quantity
+        @param price
+
+    public Product(String name, int quantity, double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    @return
+
+    public
 
 }
