f3e006a65b1e3def06e16980ce8defe79e994494
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-06 13:59:01 -0300

    Feat: adicionar um metodos para listar os produtos do inventátio

---
+++ src/InventoryManager.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class InventoryManager {
+    private List<Product> products;
+
+    public InventoryManager() {
+        this.products = new ArrayList<>();
+    }
+}
