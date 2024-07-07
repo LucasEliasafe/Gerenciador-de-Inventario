a427f14c16b0fb9bb3578c10dea8f7b65356650e
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-07 17:32:02 -0300

    Feat: Adicionar os switch case para o menu

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,18 @@ public class Main {
         manager.loadFromFile(filename);
 
         while (true) {
-            System.out.print
+            System.out.println("\n--- Menu Gerenciador ");
+            System.out.println("1. Adicionar Produto");
+            System.out.println("2. Listar Produtos");
+            System.out.println("3. Atualizar Produto");
+            System.out.println("4. Excluir Produto");
+            System.out.println("5. Salvar e Sair");
+            System.out.println("Escolha uma opção: ");
+
+            int option = scanner.nextInt();
+            scanner.nextLine();
+
+
         }
 
 
