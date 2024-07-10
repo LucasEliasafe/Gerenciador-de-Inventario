bfb0741d37e8c7b3c13d6b75c8b11e1a67df0d1b
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-10 20:25:47 -0300

    Feat:Criar os métodos de venda dos produtos

--- src/Sale.java
+++ src/Sale.java
@@ -1,2 +1,15 @@
+import java.time.LocalDate;
+
 public class Sale {
+    private Product product;
+    private int quantitySold;
+    private LocalDate date;
+
+    public Sale(Product product, int quantitySold, LocalDate date) {
+        this.product = product;
+        this.quantitySold = quantitySold;
+        this.date = date;
+    }
+
+
 }
